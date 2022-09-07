afe504f6393c2718cde74dd24ad127b397ce872a
kexy.huang <kexy.huang@nadisystem.com>
2022-09-07 10:53:18 +0800

    uploader (100%)

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -3,7 +3,8 @@ import React, {
   useRef,
   useState,
   MouseEvent,
-  useEffect
+  useEffect,
+  ChangeEvent
 } from "react";
 import "./../styles/Template.scss";
 
@@ -14,47 +15,57 @@ export default (props: Props) => {
   const textRef = useRef(null);
   const [base64Data, setBase64Data] = useState<string[]>([]);
 
-const handleFileSelect = (evt) => {
-  console.log('evt', evt)
-  var fileList = [...evt.target.files];
-  fileList.forEach((eachFile:File)=>{
-    var reader = new FileReader();
-    reader.readAsBinaryString(eachFile);
-    reader.onload = (function(theFile) {
-      console.log('theFile', theFile)
-      return function(e) {
-        if(!e.target) return;
-        var binaryData:any = e.target.result;
-        //Converting Binary Data to base 64
-        var base64String = window.btoa(binaryData);
-        //showing file converted to base64
-        if(!textRef.current) return;
-        // textRef.current.value = base64String;
-        // console.log('base64String', base64String)
-        const tempData = [...base64Data];
-        // if(!tempData) return;
-        tempData.push(base64String)
-        setBase64Data(tempData)
-        alert('File converted to base64 successfuly!\nCheck in Textarea');
+  useEffect(() => {
+    if (base64Data.length) {
+      const obj = {
+        dataList: base64Data,
+        dataLength: base64Data.length
       };
-    })(eachFile);
-    // Read in the image file as a data URL.
-    
+      // const content = JSON.stringify(base64Data)
+      const content = JSON.stringify(base64Data);
+      // console.log('content', content)
+      let a = document.createElement("a");
+      let file = new Blob([content], { type: "text/json" });
+      a.href = URL.createObjectURL(file);
+      a.download = `eventLog_${"searchTimeFileName"}.json`;
+      a.click();
+    }
+  }, [base64Data]);
 
-  })
+  const handleFileSelect = (evt: any) => {
+    console.log("evt", evt);
+    var fileList = [...evt.target.files];
+    const tempData = [...base64Data];
+    fileList.forEach((eachFile: File) => {
+      var reader = new FileReader();
+      reader.readAsBinaryString(eachFile);
+      reader.onload = (function (theFile) {
+        console.log("theFile", theFile);
+        return function (e) {
+          if (!e.target) return;
+          var binaryData: any = e.target.result;
+          //Converting Binary Data to base 64
+          var base64String = window.btoa(binaryData);
+          //showing file converted to base64
+          if (!textRef.current) return;
 
-  const content = JSON.stringify(base64Data)
-  let a = document.createElement('a')
-  let file = new Blob([content], { type: 'text/json' })
-  a.href = URL.createObjectURL(file)
-  a.download = `eventLog_${'searchTimeFileName'}.json`
-  a.click()
-
-}
+          tempData.push(base64String);
+          setBase64Data(tempData);
+        };
+      })(eachFile);
+    });
+  };
 
   return (
     <div className="_container">
-      <input type="file" id="files" name="files" multiple={true}  onChange={(evt)=>handleFileSelect(evt)} />
+      <input
+        type="file"
+        id="files"
+        name="files"
+        multiple={true}
+        accept="image/*"
+        onChange={(evt) => handleFileSelect(evt)}
+      />
       <br />
       <textarea id="base64" rows={5} ref={textRef}></textarea>
     </div>
