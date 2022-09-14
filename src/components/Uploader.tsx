ea9b687c11081537bc2bfb1e743ae5ae23610832
kexy.huang <kexy.huang@nadisystem.com>
2022-09-14 13:51:32 +0800

    update

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -20,7 +20,7 @@ type Props = {};
 export default (props: Props) => {
   const {} = props;
   const [fileCount, setFileCount] = useState<number>(0);
-  const [fileList, setFileList] = useState<File[]>();
+  const [fileList, setFileList] = useState<File[]>([]);
   const [zipName, setZipName] = useState<string>("zipFile");
   const [imageQuality, setImageQuality] = useState<number>(1);
   const [format, setFormat] = useState<string>("jpeg");
@@ -29,8 +29,32 @@ export default (props: Props) => {
   const [qualityMenuShow, setQualityMenuShow] = useState<Boolean>(false);
   const [formatMenuShow, setFormatMenuShow] = useState<Boolean>(false);
   const [ruleMenuShow, setRuleMenuShow] = useState<Boolean>(false);
+  const [sizeLimit, setSizeLimit] = useState<number>(1);
 
-  function processFile(file: File) {
+  const roundTo = ( num:number, decimal:number ) => { 
+      return Math.round( ( num + Number.EPSILON ) * Math.pow( 10, decimal ) )/ Math.pow( 10, decimal ); 
+  }
+
+  const getMaxFile = (filePack:File[]) => {
+    let b = Math.max(...filePack?.map(f=>f.size))
+    console.log('b', b)
+    console.log('b_kb', b/1024)
+    console.log('b_mb', b/1024/1024)
+    let maxFile = filePack.filter((item)=>{
+      return item.size === Math.max(...filePack?.map(f=>f.size))
+    })[0]
+    console.log('maxFile', maxFile)
+    return maxFile
+    // return maxFile.size;
+    
+  }
+
+  function processFile(file: File, formatParam:string, imageQualityParam:number) {
+    console.log('=======================================')
+    console.log('file', file)
+    console.log('formatParam', formatParam)
+    console.log('imageQualityParam', imageQualityParam)
+    console.log('=======================================')
     if (!file) {
       return;
     }
@@ -59,7 +83,8 @@ export default (props: Props) => {
 
             canvas.toBlob(
               function (blob: any) {
-                resolve(URL.createObjectURL(blob));
+                console.log('blob', blob)
+                resolve({imageUrl: URL.createObjectURL(blob), size:blob.size, format: format});
               },
               `image/${format}`,
               imageQuality
@@ -67,19 +92,20 @@ export default (props: Props) => {
             // canvas.toDataURL("image/webp");
           });
         })
-        .then((imageURL) => {
+        .then((imageObj:any) => {
+          console.log('imageObj', imageObj)
           // Load image for display on the page
           return new Promise(function (resolve, reject) {
             let scaledImg = new Image();
 
             scaledImg.addEventListener("load", function () {
-              resolve({ imageURL, scaledImg });
+              resolve({ imageURL: imageObj.imageURL, scaledImg, size: imageObj.size});
             });
 
-            scaledImg.setAttribute("src", imageURL as string);
+            scaledImg.setAttribute("src", imageObj.imageURL as string);
 
             // console.log("imageURL", imageURL);
-            resolveOuter(imageURL as string[]);
+            resolveOuter(imageObj);
           });
         })
         .then(async function (data) {
@@ -88,19 +114,65 @@ export default (props: Props) => {
         });
     });
   }
-  const handleFileSelect = (evt: any) => {
+  const handleFileSelect = async (evt: any) => {
     var eventfileList = [...evt.target.files];
     console.log("eventfileList", eventfileList);
     setFileCount(eventfileList.length);
     setFileList(eventfileList);
+    let maxFile = getMaxFile(eventfileList)
+    if(!maxFile) return;
+    console.log('maxFile.size', maxFile.size)
+    console.log('maxFile.size/1024_kb', maxFile.size/1024)
+    console.log('maxFile.size/1024/1024_mb', maxFile.size/1024/1024)
+    let maxFileProcessResult = maxFile.size/1024/1024;
+
+    // let aaaa = getRecommendQuality(maxFile, format, imageQuality)
+    // console.log('aaaa', aaaa)
+    // if(aaaa > sizeLimit) {
+
+    // }
+    // let testProcessJ = await processFile(maxFile, 'jpeg');
+    // console.log('testProcessJ', testProcessJ)
+    // let testProcessP = await processFile(maxFile, 'png');
+    // console.log('testProcessP', testProcessP)
+    // let testProcessW = await processFile(maxFile, 'webp');
+    // console.log('testProcessW', testProcessW)
   };
+
+  const getRecommendQuality = async(maxFile:File, formatParam:string, imageQualityParam:number) => {
+    console.log('===============imageQualityParam', imageQualityParam)
+    // if(imageQualityParam === 0.1 && (format === 'jpeg' || format === 'png')) {
+    //   // setFormat('webp');
+    //   getRecommendQuality(maxFile, 'webp', roundTo(imageQualityParam, 1));
+    // } else if(imageQualityParam===0.1 && format === 'webp') {
+    //   alert('還是很大 放棄')
+    // };
+    // if(imageQualityParam <= 0.1) return;
+    if(imageQualityParam <= 0) return;
+    console.log('__________________imageQualityParam', imageQualityParam)
+    console.log('--------------------------------------------------------------')
+    let testProcess:any = await processFile(maxFile, format, imageQualityParam);
+    console.log('testProcess', testProcess)
+    console.log('testProcess.size/1024/1024', testProcess.size/1024/1024)
+    console.log('sizeLimit', sizeLimit)
+    if(testProcess.size/1024/1024 > sizeLimit) {
+      console.log('太大了')
+      console.log('imageQualityParam', imageQualityParam)
+      getRecommendQuality(maxFile, formatParam, roundTo(imageQualityParam-0.1, 1));
+    } else {
+      console.log('可以')
+      // setImageQuality(imageQualityParam > 0.1 ? imageQualityParam : 0.1)
+      setImageQuality(imageQualityParam)
+    }
+    return testProcess;
+  }
 
   const handleFileResize = () => {
     if(!fileList) return;
     const results = fileList.map(async (eachFile: File) => {
-      const res = await processFile(eachFile);
+      const res:any = await processFile(eachFile, format, imageQuality);
       console.log("res", res);
-      return { blobUrl: res, fileName: eachFile.name };
+      return { blobUrl: res.imageUrl, fileName: eachFile.name };
     });
 
     var zip = new JSZip();
@@ -112,11 +184,16 @@ export default (props: Props) => {
       console.log("___________item", item);
       return item.then((obj) => {
         // var filename = `${obj.fileName}-${index + 1}.${format}`;
-        var filename = `${obj.fileName}.${format}`;
+        let fileNameFragment = obj.fileName.split(".");
+        console.log('fileNameFragment', fileNameFragment)
+        let originalFilenameExtension = fileNameFragment[fileNameFragment.length-1]
+        console.log('originalFilenameExtension', originalFilenameExtension)
+        let filename = `${obj.fileName.replace(originalFilenameExtension, format)}`;
         console.log("obj", obj);
         JSZipUtils.getBinaryContent(
           obj.blobUrl,
           function (err: any, data: ArrayBuffer) {
+            console.log('=====================data', data)
             if (err) {
               throw err; // or handle the error
             }
@@ -133,7 +210,7 @@ export default (props: Props) => {
     });
   };
 
-  const qualityList = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
+  const qualityList = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'];
   const changeQuality = (item: number) => {
     setImageQuality(item);
     setQualityMenuShow(false);
@@ -166,6 +243,7 @@ export default (props: Props) => {
       <div className="area zip_file_name_area">
         <div className="title">壓縮檔名稱</div>
         <input
+          type="text"
           className="file_name"
           onChange={(e) => setZipName(e.target.value)}
           value={zipName}
@@ -206,6 +284,7 @@ export default (props: Props) => {
         <div className="area">
           <div className="title">自定義檔名</div>
           <input
+            type="text"
             className="custom_image_name"
             onChange={(e) => setCustomName(e.target.value)}
             value={customName}
@@ -213,6 +292,17 @@ export default (props: Props) => {
           />
         </div>
       ) : null}
+
+      {/* <div className="area">
+          <div className="title">檔案大小限制</div>
+          <input
+            type="number"
+            className="custom_image_name"
+            onChange={(e) => setSizeLimit(Number(e.target.value))}
+            value={sizeLimit}
+            min={0.2}
+          /> <div className="unit">MB</div>
+        </div> */}
       <button onClick={()=>handleFileResize()}>轉換</button>
     </div>
   );
